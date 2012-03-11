09d77019be71204d55a8a127a6a2063dcfcf34f6
SND\imli_cp <SND\imli_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2012-03-11 22:10:40 +0000

    git-svn-id: https://imli.svn.codeplex.com/svn@10921 3b72ae23-dab2-410c-8069-39ac8a3268c2

---
+++ Master thesis/Sensor network 2/Settings.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SensorNetwork
+{
+        /// <summary>
+        /// Class for retreiving information from ServiceSettings.config file
+        /// </summary>
+    public class Settings
+    {
+
+        public int GetAreaSize()
+        {
+            int areaSize;
+            Int32.TryParse(System.Configuration.ConfigurationManager.AppSettings.Get("NetworkAreaSize"), out areaSize);
+            return areaSize;
+        }
+
+        public int GetMaxNodeCount()
+        {
+            int nodeCount;
+            Int32.TryParse(System.Configuration.ConfigurationManager.AppSettings.Get("NetworkMaxNodeCount"), out nodeCount);
+            return nodeCount;
+        }
+
+        public int GetInitialNodeCount()
+        {
+            int nodeCount;
+            Int32.TryParse(System.Configuration.ConfigurationManager.AppSettings.Get("InitialNodeCount"), out nodeCount);
+            return nodeCount;
+        }
+
+        public int GetNetworkID()
+        {
+            int networkID;
+            Int32.TryParse(System.Configuration.ConfigurationManager.AppSettings.Get("NetworkID"), out networkID);
+            return networkID;
+        }
+
+        public string GetLogPath()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings.Get("LogPath");
+        }
+
+        public int GetCrossNetworkNodeCount()
+        {
+            int crossNodes;
+            Int32.TryParse(System.Configuration.ConfigurationManager.AppSettings.Get("CrossNetworkNodes"), out crossNodes);
+            return crossNodes;
+        }
+    }
+    
+}
